a43e861446aa2859619ac6561fb96be6ea5da501
Kirk Diggler <kirklgilmore@gmail.com>
2012-05-20 13:48:18 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homecal::Application.config.secret_token = 'c4f6639da1a30ddcfefbfd8fb0e3ae4a2a1fc1d78fe76c039e7a3de1232bd74512a085d5547ca8a1deabaa67f8b279a7f4680d7b4cb4ea3e1e252057b81bbbd6'
